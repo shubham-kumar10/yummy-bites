0e0886c9dc9d6fdb2c80ef963844bfababcbcc9e
Kumar, Shubham <shubham.snu@gmail.com>
2020-02-07 18:15:05 +0530

    User services for login signup added

---
+++ src/app/site/user-service.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, ɵConsole } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { User } from './User/User';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserServiceService {
+  url: string = environment.baseUrl + "auth-service/";
+
+  constructor(private router: Router, private _httpClient: HttpClient) { }
+
+  addUser(user: User): Observable<any> {
+    //this.userList.push(user);
+    console.log("Inside add user of service ")
+    console.log(user)
+    return this._httpClient.post<any>(this.url + "users", user)
+    this.router.navigate(['login']);
+  }
+}
