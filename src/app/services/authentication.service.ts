32300f47841d4af2b0ff1e4a33b0879566750bad
Kumar, Shubham <shubham.snu@gmail.com>
2020-02-09 18:27:46 +0530

    Auth service

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { UserServiceService } from '../site/user-service.service';
+import { Router } from '@angular/router';
+
+import { MenuItemService } from './menu-item.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private _httpClient: HttpClient, private userService: UserServiceService, public router: Router) { }
+
+  loggedInUser = { loggedOut: true };
+  validCredentials: boolean = true;
+  accessToken: string; // JWT token
+  redirectUrl = '/';
+  loggedIn: boolean = false;
+  private authenticationApiUrl = environment.baseUrl + "auth-service/";
+  private token: string;
+  username: string;
+  userId: number = 0;
+  public setToken(token: string) {
+    this.token = token;
+  }
+  public getToken() {
+    return this.token;
+  }
+
+  public setUserId() {
+    let sum: number = 0;
+    for (let i = 0; i < this.username.length; i++) {
+      this.userId = this.username.charCodeAt(i) + this.userId;
+    }
+  }
+  authenticate(user: string, password: string): Observable<any> {
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', 'Basic ' + btoa(user + ':' + password));
+    return this._httpClient.get(this.authenticationApiUrl + "authenticate", { headers })
+  }
+
+  logout() {
+    this.loggedIn = false;
+    this.setToken(null);
+    this.userId = null;
+  }
+
+}
