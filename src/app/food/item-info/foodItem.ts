05213ed3a4980ef0712c8f022f1a50d8b8306828
Kumar, Shubham <shubham.snu@gmail.com>
2020-02-11 18:25:58 +0530

    food interface added

---
+++ src/app/food/item-info/foodItem.ts
@@ -0,0 +1,10 @@
+export interface foodItem{
+    id:number;
+    name:string;
+    price:number;
+    active:boolean;
+    dateOfLaunch:Date;
+    category:string;
+    freeDelivery:boolean;
+    imageUrl:string;
+}
