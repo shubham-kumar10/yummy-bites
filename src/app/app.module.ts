25a9adec7cc0384a629874067b2dbc546a557f1a
Kumar, Shubham <shubham.snu@gmail.com>
2020-02-14 18:41:56 +0530

    app.module update

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ItemInfoComponent } from './food/item-info/item-info.component';
+import { MenuComponent } from './food/menu/menu.component';
+import { SearchComponent } from './food/search/search.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CartComponent } from './shopping/cart/cart.component';
+import { ItemEditComponent } from './food/item-edit/item-edit.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './site/signup/signup.component';
+import { LoginComponent } from './site/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const appRoutes: Routes = [
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemInfoComponent,
+    MenuComponent,
+    SearchComponent,
+    CartComponent,
+    ItemEditComponent,
+    SignupComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
