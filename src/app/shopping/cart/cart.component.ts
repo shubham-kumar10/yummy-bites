66bbbd6c8e5d2a0444bce69ca7c92704298ce9b5
Kumar, Shubham <shubham.snu@gmail.com>
2020-02-15 18:37:50 +0530

    cart added

---
+++ src/app/shopping/cart/cart.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Cart } from './cart';
+import { CartServiceService } from '../cart-service.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  cart: Cart;
+
+  empty: boolean = false;
+  remove: boolean = false;
+  constructor(private cartService: CartServiceService) { }
+
+  ngOnInit() {
+    this.cartService.getAllCartItems().subscribe(data => {
+      this.cart = data;
+      if (this.cart.total == 0)
+        this.empty = true;
+      console.log(this.cart)
+    });
+  }
+
+  onDeleteCart(id: number) {
+    console.log("Inside delete Cart item")
+    this.cartService.deleteCartItem(id).subscribe();
+    this.remove = true;
+    this.cartService.getAllCartItems().subscribe(data => {
+      this.cart = data;
+      if (this.cart.total == 0)
+        this.empty = true;
+      console.log(this.cart)
+    });
+  }
+}
