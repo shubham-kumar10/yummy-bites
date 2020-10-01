436e3b2ad83724658852a39e8e961f4312814213
Shubham Kumar <shubham.snu@gmail.com>
2020-10-02 01:41:24 +0530

    Add item card added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // baseUrl: 'https://yummy-bites-backend.herokuapp.com/'
-  baseUrl: 'http://localhost:8083/'
+  baseUrl: 'https://yummy-bites-backend.herokuapp.com/'
+  // baseUrl: 'http://localhost:8083/'
 };
 
 /*
