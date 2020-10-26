90516b26dde88b2d082743341bbb60275c87b7dd
Alexandre Insua <alexandreinsua@gmail.com>
2020-10-26 22:46:02 +0100

    fixed conflicts in app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,17 +2,10 @@ import { TestBed, async, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-<<<<<<< HEAD
 import { AuthenticationService } from './shared/services/authentication.service';
 import { MenuItemService } from './shared/services/menu-item.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { Router } from '@angular/router';
-=======
-import { AuthenticationService } from './services/authentication.service';
-import { MenuItemService } from './services/menu-item.service';
-import { HttpClient, HttpHandler } from '@angular/common/http';
-import { FoodServiceService } from './food/food-service.service';
->>>>>>> 966acfb7f5b7ae194f0ffb4ed6d67b9699ad8258
 
 describe('Tests for AppComponent', () => {
   beforeEach(async(() => {
@@ -28,10 +21,6 @@ describe('Tests for AppComponent', () => {
         MenuItemService,
         HttpClient,
         HttpHandler,
-<<<<<<< HEAD
-=======
-        FoodServiceService
->>>>>>> 966acfb7f5b7ae194f0ffb4ed6d67b9699ad8258
       ]
     }).compileComponents();
   }));
@@ -43,52 +32,30 @@ describe('Tests for AppComponent', () => {
       expect(app).toBeTruthy();
     });
 
-<<<<<<< HEAD
     it(`should have as title 'Yummy Bites'`, () => {
       const fixture = TestBed.createComponent(AppComponent);
       const app = fixture.debugElement.componentInstance;
       expect(app.title).toEqual('Yummy Bites');
     });
   });
-=======
-    it(`should have as title 'truYum'`, () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.debugElement.componentInstance;
-      expect(app.title).toEqual('truYum');
-    });
-  })
->>>>>>> 966acfb7f5b7ae194f0ffb4ed6d67b9699ad8258
 
   // it('should render title in a h2 tag', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
   //   const compiled = fixture.debugElement.nativeElement;
-<<<<<<< HEAD
   //   expect(compiled.querySelector('h2').textContent).toContain('Yummy Bites');
-=======
-  //   expect(compiled.querySelector('h2').textContent).toContain('truYum');
->>>>>>> 966acfb7f5b7ae194f0ffb4ed6d67b9699ad8258
   // });
 
   class MockAuthService extends AuthenticationService {
     loggedIn = false;
   }
 
-<<<<<<< HEAD
   describe('Test for LoggedIn function', () => {
-    // tslint:disable-next-line: prefer-const
+
     let http: HttpClient, router: Router, food: MenuItemService;
     const auth = new MockAuthService(http, router);
     const app = new AppComponent(auth, router, food);
     let result: boolean;
-=======
-
-  describe('Test for LoggedIn function', () => {
-    let http, user, router;
-    let auth = new MockAuthService(http, user, router), food;
-    let app = new AppComponent(auth, router, food);
-    let result;
->>>>>>> 966acfb7f5b7ae194f0ffb4ed6d67b9699ad8258
 
     // it(`should return 'null'`, inject([AuthenticationService], () => {
     //   auth.loggedIn = undefined;
@@ -107,9 +74,5 @@ describe('Tests for AppComponent', () => {
       result = app.loggedIn();
       expect(result).toBeFalsy();
     }));
-<<<<<<< HEAD
   });
-=======
-  })
->>>>>>> 966acfb7f5b7ae194f0ffb4ed6d67b9699ad8258
 });
